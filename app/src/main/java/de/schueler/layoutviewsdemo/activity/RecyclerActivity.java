42b2fe4c5f4db9dde341788d68b66802af203454
Thomas Schaller <thomas-schaller2000@gmx.de>
2016-12-04 10:58:14 +0100

    Add recycler view

--- app/src/main/java/de/schueler/layoutviewsdemo/activity/RecyclerActivity.java
+++ app/src/main/java/de/schueler/layoutviewsdemo/activity/RecyclerActivity.java
@@ -2,13 +2,17 @@ package de.schueler.layoutviewsdemo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import de.schueler.layoutviewsdemo.R;
 
 public class RecyclerActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
     }
 }
