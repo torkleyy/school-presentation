6946ebd2d38faaea6697dc6185b67d0b45d955ef
Thomas Schaller <thomas-schaller2000@gmx.de>
2016-12-04 11:18:30 +0100

    Add descriptionId to topic data and refactor image path to id

--- app/src/main/java/de/schueler/layoutviewsdemo/data/Topic.java
+++ app/src/main/java/de/schueler/layoutviewsdemo/data/Topic.java
@@ -2,18 +2,24 @@ package de.schueler.layoutviewsdemo.data;
 
 public final class Topic {
     private final int titleId;
-    private final String imagePath;
+    private final int imageId;
+    private final int descriptionId;
 
-    public Topic(int titleId, String imagePath) {
+    public Topic(int titleId, int imageId, int descriptionId) {
         this.titleId = titleId;
-        this.imagePath = imagePath;
+        this.imageId = imageId;
+        this.descriptionId = descriptionId;
     }
 
     public int getTitleId() {
         return titleId;
     }
 
-    public String getImagePath() {
-        return imagePath;
+    public int getImageId() {
+        return imageId;
+    }
+
+    public int getDescriptionId() {
+        return descriptionId;
     }
 }
